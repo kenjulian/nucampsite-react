18ed4afd13c5c873b8252e75b587c391a636a0bf
kenjulian <kenneth.julian@yahoo.com>
2022-04-07 12:04:10 -0400

    lifted state from directory to parent App; passed it down as props changing references from state to props in the directory component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, {Component} from 'react';//imports w/o ./ are from modules from th
 import {Navbar, NavbarBrand} from 'reactstrap';//components
 import Directory from './components/DirectoryComponent'
 import './App.css';
+import {CAMPSITES} from './shared/campsites'
 
 class App extends Component {
+    constructor(props) {
+        super(props) 
+        this.state = {
+            campsites: CAMPSITES
+        } 
+
+    }
   render() {
       return (
           <div className="App">
@@ -12,7 +20,7 @@ class App extends Component {
                   <NavbarBrand href="/">NuCamp</NavbarBrand>
               </div>
               </Navbar>
-              <Directory />
+              <Directory campsites={this.state.campsites} />
           </div>
       );
   }
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,49 +1,86 @@
 import React, {Component} from 'react';
+import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
 
 class Directory extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: [
-                {
-                    id: 0,
-                    name: 'React Lake Campground',
-                    image: 'assets/images/react-lake.jpg',
-                    elevation: 1233,
-                    description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
-                },
-                {
-                  id: 1,
-                  name: 'Chrome River Campground ',
-                  image: 'assets/images/chrome-river.jpg',
-                  elevation: 877,
-                  description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
-                },
-                {
-                    id: 2,
-                    name: 'Breadcrumb Trail Campground',
-                    image: 'assets/images/breadcrumb-trail.jpg',
-                    elevation: 2901,
-                    description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground."
-                },
-                {
-                    id: 3,
-                    name: 'Redux Woods Campground',
-                    image: 'assets/images/redux-woods.jpg',
-                    elevation: 42,
-                    description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
-                }
-            ]
+            selectedCampsite: null
+            // campsites: [
+            //     {
+            //         id: 0,
+            //         name: 'React Lake Campground',
+            //         image: 'assets/images/react-lake.jpg',
+            //         elevation: 1233,
+            //         description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
+            //     },
+            //     {
+            //       id: 1,
+            //       name: 'Chrome River Campground ',
+            //       image: 'assets/images/chrome-river.jpg',
+            //       elevation: 877,
+            //       description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
+            //     },
+            //     {
+            //         id: 2,
+            //         name: 'Breadcrumb Trail Campground',
+            //         image: 'assets/images/breadcrumb-trail.jpg',
+            //         elevation: 2901,
+            //         description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground."
+            //     },
+            //     {
+            //         id: 3,
+            //         name: 'Redux Woods Campground',
+            //         image: 'assets/images/redux-woods.jpg',
+            //         elevation: 42,
+            //         description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
+            //     }
+            // ]
         };   
     }
 
+        onCampsiteSelect(campsite) {
+            //this method changes the value of the state (selectedCampsite property)
+            this.setState({selectedCampsite: campsite});
+            //prevents having to directly change state: this.state.selectedCampsite = campsite
+        }
+
+        renderSelectedCampsite(campsite) {
+            //will either render a campsite; else, it'll render nothing(empty div)
+            if (campsite) {
+                return (
+                    <Card>
+                        <CardImg top src={campsite.image} alt={campsite.name} />
+                        <CardBody>
+                            <CardTitle>{campsite.name}</CardTitle>
+                            <CardText>{campsite.description}</CardText>
+
+                        </CardBody>
+                    </Card>
+                )
+            }
+
+            return <div />
+        }
+
     render() {
-        const directory = this.state.campsites.map(campsite => {
+        //changed this.state.campsites to this.props.campsites bc it no longer has state data but data is being passed down from parent component(App)
+        const directory = this.props.campsites.map(campsite => {
             return (
-                <div key={campsite.id} className="col">
-                    <img src={campsite.image} alt={campsite.name} />
-                    <h2>{campsite.name}</h2>
-                    <p>{campsite.description}</p>
+                <div key={campsite.id} className="col-md-5 m-1">
+                    {/* campsite from props */}
+                    {/* this is set up so that when a card is clicked, the campsite gets set as selectedCampsite in the local state */}
+
+                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{campsite.name}</CardTitle>
+
+                        </CardImgOverlay>
+                        
+
+                    </Card>
+                    
                 </div>
             )
         })
@@ -52,10 +89,17 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
+                <div className='row'>
+                    <div className="col-md-5 m-1">
+                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
+                    </div>
+                </div>
             </div>
         );
     }
 
 }
+
+
 
 export default Directory;
